9daf05c3e2ee77b166ab2cb69bcebf409ef062a5
Sculptex <sculptex@users.noreply.github.com>
2022-05-15 15:00:04 +0100

    Create init-hestia.sh

---
+++ init-hestia.sh
@@ -0,0 +1,7 @@
+source config.sh
+
+wget https://raw.githubusercontent.com/hestiacp/hestiacp/release/install/hst-install.sh
+
+sudo bash hst-install.sh --interactive no --email $email --password $password --hostname $url --mysql no --phpfpm yes --multiphp no --apache no --named no --exim no --dovecot no --sieve no --clamav no --spamassassin no --iptables no --fail2ban no --quota no --api no -f
+v-add-letsencrypt-host
+sed -i "s|/home/%user%/.composer:/home/%user%/web/|/home/%user%/.composer:/home/%user%/.zcn:/home/%user%/web/|" /usr/local/hestia/data/templates/web/php-fpm/default.tpl
