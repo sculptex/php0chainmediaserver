b419dcbba01bbb00a45d6a5326c4b5d019cb72b5
Sculptex <sculptex@users.noreply.github.com>
2022-05-15 15:00:25 +0100

    Create initweb-hestia.sh

---
+++ initweb-hestia.sh
@@ -0,0 +1,6 @@
+source config.sh
+
+chown $user:$user *.php
+rm /home/$user/web/$url/public_html/*
+cp *.php /home/$user/web/$url/public_html
+cp /home/$user/web/$url/public_html/mediaserve.php /home/$user/web/$url/public_html/index.php
